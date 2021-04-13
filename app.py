83e44fc7a3c39a7bfaea2ea23c8cd04c81585506
wenjun0903 <55336353+wenjun0903@users.noreply.github.com>
2021-04-13 11:42:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,12 @@ from sklearn.preprocessing import MinMaxScaler
 import datetime
 from dateutil.relativedelta import relativedelta
 import matplotlib.pyplot as plt
+import logging
 
+
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
  
 
